ff081b9cabb9561cc8cfe4268db6b1b3809e50c6
Custy <95989523+Custyyyy@users.noreply.github.com>
2022-01-10 16:55:06 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,78 @@
+const showTimer = true;
+// Add list of names here
+const namesList = [
+'Edward',
+'Jhonny',
+'Jenny', 
+'Philip',
+'Stepa',
+'David',
+'Charloet',
+];
+
+// Default variables
+let i = 0;
+let x = 0;
+let intervalHandle = null;
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const headerOne = document.getElementById('headerNames');
+
+// Optional countdown timer
+// Add zero in front of numbers < 10
+function checkSecond(sec) {
+  if (sec < 5   && sec >= 0) {
+sec = "0" + sec;
+} 
+  if (sec < 0) {
+sec = "59";
+}
+  return sec;
+}
+// Start or stop the name shuffle on the button click
+startButton.addEventListener('click', function() {
+this.style.display = "none";
+stopButton.style.display = "block";
+intervalHandle = setInterval(function () {
+headerNames.textContent = namesList[i++ % namesList.length];
+}, 50);
+if (showTimer===true) {
+timerWrapper.classList.remove('visible');
+}
+});
+stopButton.addEventListener('click', function() {
+this.style.display = "none";
+startButton.style.display = "block";
+clearInterval(intervalHandle);
+timer.innerHTML = time;
+if (showTimer===true) {
+timerWrapper.classList.add('visible');
+}
+startTimer();
+});
+
+// Allow use of spacebar to start/stop name shuffle
+document.body.onkeyup = function(e) {
+if (e.keyCode == 32) {
+if (x%2===0) {
+startButton.style.display = "none";
+stopButton.style.display = "block";
+intervalHandle = setInterval(function () {
+headerNames.textContent = namesList[i++ % namesList.length];
+}, 50);
+if (showTimer===true) {
+timerWrapper.classList.remove('visible');
+}
+} else {
+startButton.style.display = "block";
+stopButton.style.display = "none";
+clearInterval(intervalHandle);
+timer.innerHTML = time;
+if (showTimer===true) {
+timerWrapper.classList.add('visible');
+}
+startTimer();
+}
+x++; 
+}
+}
